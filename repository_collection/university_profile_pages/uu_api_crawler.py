94541f1cbf14decd9a9307588860d5868839d4c9
chrisslewe <christopher.slewe@gmail.com>
2021-04-12 11:04:50 +0200

    Add write to csv uu profile crawler

--- repository_collection/university_profile_pages/uu_api_crawler.py
+++ repository_collection/university_profile_pages/uu_api_crawler.py
@@ -1,15 +1,16 @@
-import pandas as pd
 import json
-import requests
 import time
+from pathlib import Path
+
+import pandas as pd
+import requests
+
 
 def get_employees_url(faculty_number):
     url = f"https://www.uu.nl/medewerkers/RestApi/Public/GetEmployeesOrganogram?f={faculty_number}&l=EN&fullresult=true"
     json_nested  = requests.get(url)
     df = pd.DataFrame(json_nested.json()["Employees"])
     return df["Url"]
-
-#print(get_employees_url(3))
 
 
 def get_employee_links(url):
@@ -21,6 +22,8 @@ def get_employee_links(url):
         return  API_json["Employee"]['Links']
     except:
         return "no links in user API"
+
+
 def get_employee_specific_link(url, search_query):
     time.sleep(1)#avoid ban by adding time between requests
     API_link = requests.get(f"https://www.uu.nl/medewerkers/RestApi/Public/getEmployeeData?page={url}")
@@ -31,9 +34,10 @@ def get_employee_specific_link(url, search_query):
         for link in employee_link_list:
             if  search_query in link['Url']:
                 return item['Url']
-        return "query not found in links"
+        return None
     except:
-        return "no links in user API"
+        return None
+
 
 def get_employee_github(url):
     time.sleep(1)#avoid ban by adding time between requests
@@ -45,9 +49,11 @@ def get_employee_github(url):
         for link in employee_link_list:
             if "github" in link['Url']:
                 return link['Url']
-        return "no github page in user links"
+        return None
     except:
-        return "no links in user API"
+        return None
+
+
 def get_employee_profile_mention(url, query):
     query = query.lower() # Casing doesn't matter for search
     time.sleep(1)#avoid ban by adding time between requests
@@ -62,6 +68,7 @@ def get_employee_profile_mention(url, query):
     except:
         return "no profile description in user API"
 
+
 employee_links = []
 employee_github = []
 employee_link_query_test = []
@@ -69,15 +76,20 @@ employee__profile_query_test = []
 urls_for_test = list(get_employees_url(3).head())
 urls_for_test.append("jdebruin1")  #contains many of these features, so a good example for testing
 for url in urls_for_test:
-    employee_links.append([url, get_employee_links(url)])
+    #employee_links.append([url, get_employee_links(url)])
     employee_github.append([url, get_employee_github(url)])
-    employee_link_query_test.append([url, get_employee_specific_link(url, "orcid")])
-    employee__profile_query_test.append([url, get_employee_profile_mention(url, "open science")])
+    #employee_link_query_test.append([url, get_employee_specific_link(url, "orcid")])
+    #employee__profile_query_test.append([url, get_employee_profile_mention(url, "open science")])
 
+employee_github_pd = pd.DataFrame(employee_github,  columns=["uu_user_id", "github_user_url"])
+employee_github_pd = employee_github_pd.dropna()
+print(employee_github_pd)
 
+p = Path("repository_collection", "university_profile_pages","results")
+p.mkdir(parents=True, exist_ok=True)
+employee_github_pd.to_csv(Path("repository_collection", "university_profile_pages","results", "employees_github_urls.csv"), index = False)
 
-
-print(employee_links)
-print(employee_github)
-print(employee_link_query_test)
-print(employee__profile_query_test)
+#print(employee_links)
+#print(employee_github)
+#print(employee_link_query_test)
+#print(employee__profile_query_test)
