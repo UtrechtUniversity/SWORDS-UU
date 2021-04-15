1314671d6525cf4a8258587bbfa2202a3a64c6ae
beld <belfami@gmail.com>
2021-04-15 18:07:32 +0200

    add working solution for PURE data

--- repository_collection/pure/pure.py
+++ repository_collection/pure/pure.py
@@ -1,7 +1,31 @@
-import rispy
+from pathlib import Path
 
-filepath = "Pure_100321.ris" 
+import rispy
+import pandas as pd
+
+def get_username_from_text(text):
+    for word in text:
+        if "github.com" in word:
+            split = word.split("github.com/")
+            # get value after "github.com/" and split after the next slash, then the first value of that split will be the username
+            user = split[1].split("/")[0]
+            return user
+        elif "github.io" in word:
+            githubio_split = word.split(".")
+            user = githubio_split[0].split("://")[1]
+            return user
+    return None
+
+filepath = r"repository_collection\pure\Pure_100321.ris" 
+users = list()
 with open(filepath, 'r', encoding="utf8") as pure_data:
     entries = rispy.load(pure_data)
     for entry in entries:
-        print(entry)
+        user = get_username_from_text(entry.values())
+        if (user is not None):
+            users.append(user)
+            
+    pd.Series(users).to_csv(Path("repository_collection", "pure", "results", "ids_pure_users.csv"), index = False)
+    
+
+
