ff816b953d96c9307da970705bb3cde05a714cfe
beld <belfami@gmail.com>
2021-11-04 23:04:33 +0100

    Add whole phase 1 pipeline script

---
+++ collect_users/run_pipeline.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Fetching users..."
+cd methods/github_search
+pip install -r requirements.txt
+python github_search.py --topic utrecht-university --search "utrecht university"
+cd ../papers_with_code
+pip install -r requirements.txt
+python papers_with_code.py --query utrecht+university
+cd ../pure
+pip install -r requirements.txt
+python pure.py
+cd ../university_profile_pages
+pip install -r requirements.txt
+python uu_api_crawler.py
+echo "Merging users..."
+pip install -r requirements.txt
+
+cd ../../
+python scripts/merge_users.py --files methods/*/results/*.csv --output results/users_merged.csv
+echo "Enriching users..."
+python scripts/enrich_users.py --input results/users_merged.csv
+echo "Preparing user filtering..."
+python scripts/prepare_filtering.py
+echo "Pipeline run succeeded."
