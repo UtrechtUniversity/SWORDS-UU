a5e43c10a2a210f09d3004f090c60303c1a2e4a4
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-11-22 14:29:04 +0100

    Clean enrich_users and merge_users

--- collect_users/scripts/merge_users.py
+++ collect_users/scripts/merge_users.py
@@ -5,51 +5,51 @@ import glob
 
 import pandas as pd
 
-# Initiate the parser
-parser = argparse.ArgumentParser()
+if __name__ == '__main__':
 
-# Add arguments to be parsed
-parser.add_argument(
-    "--files",
-    "-f",
-    nargs="*",
-    help="set file paths to be merged. Example: methods/*/results/*.csv",
-    default="methods/*/results/*.csv")
-# python merge_users.py file1.csv file2.csv | python merge_users.py */results/*.csv
-parser.add_argument("--output",
-                    "-o",
-                    help="file name of output. Default: users_merged.csv",
-                    default="results/users_merged.csv")
+    # Initiate the parser
+    parser = argparse.ArgumentParser()
 
-# Read arguments from the command line
-args = parser.parse_args()
-print(f"File arguments: {args.files}")
+    # Add arguments to be parsed
+    parser.add_argument(
+        "--files",
+        "-f",
+        nargs="*",
+        help="set file paths to be merged. Example: methods/*/results/*.csv",
+        default="methods/*/results/*.csv")
+    parser.add_argument("--output",
+                        "-o",
+                        help="file name of output. Default: users_merged.csv",
+                        default="results/users_merged.csv")
 
-data_files = []
-for file in args.files:
-    if ("*" in file):  # workaround for Windows shell not expanding asterisk
-        data_files.extend(glob.glob(file))
-    else:
-        data_files.append(file)
+    # Read arguments from the command line
+    args = parser.parse_args()
+    print(f"File arguments: {args.files}")
 
-print(f"Parsing files for... \n {data_files}")
-df_github_names_long = pd.concat(
-    [pd.read_csv(fp) for fp in data_files],
-    axis=0,
-    keys=data_files,
-    names=["source", "row"]).reset_index("source").reset_index(drop=True)
-# lowercase to remove duplicates correctly
-df_github_names_long['github_user_id'] = df_github_names_long[
-    'github_user_id'].str.lower()
+    data_files = []
+    for file in args.files:
+        if ("*" in file
+            ):  # workaround for Windows shell not expanding asterisk
+            data_files.extend(glob.glob(file))
+        else:
+            data_files.append(file)
 
-df_users = df_github_names_long[["github_user_id", "source"]].sort_values([
-    "github_user_id", "source"
-]).drop_duplicates(["github_user_id", "source"]).reset_index(
-    drop=True
-)  # duplicates need to be dropped because one source can return a github_user_id multiple times
+    print(f"Parsing files for... \n {data_files}")
+    df_github_names_long = pd.concat(
+        [pd.read_csv(fp) for fp in data_files],
+        axis=0,
+        keys=data_files,
+        names=["source", "row"]).reset_index("source").reset_index(drop=True)
+    # lowercase to remove duplicates correctly
+    df_github_names_long['github_user_id'] = df_github_names_long[
+        'github_user_id'].str.lower()
 
-df_users['source'] = df_users['source'].map(lambda x: pathlib.PurePath(
-    x).name)  # remove file path so the column only contains the file name
+    df_users = df_github_names_long[["github_user_id", "source"]].sort_values([
+        "github_user_id", "source"
+    ]).drop_duplicates(["github_user_id", "source"]).reset_index(drop=True)
 
-df_users.to_csv(Path(args.output), index=False)
-print("Successfully merged users.")
+    df_users['source'] = df_users['source'].map(lambda x: pathlib.PurePath(
+        x).name)  # remove file path so the column only contains the file name
+
+    df_users.to_csv(Path(args.output), index=False)
+    print("Successfully merged users.")
