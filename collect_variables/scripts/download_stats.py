925e33d7423cb45aa94ef64887405a808382d3d3
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-01-17 12:00:52 +0100

    Make error more specific

--- collect_variables/scripts/download_stats.py
+++ collect_variables/scripts/download_stats.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 
             try:
                 readme = repo["readme"]
-            except Exception:
+            except KeyError:
                 continue
 
             # python
