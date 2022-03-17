de3521c3ed87b83348dc8b7f0b82604b6b24eeda
Keven Quach <belfami@gmail.com>
2022-03-17 20:20:55 +0100

    Add tests for pure.py

--- tests/test_user_methods.py
+++ tests/test_user_methods.py
@@ -8,6 +8,12 @@ from fastcore.foundation import L, AttrDict
 from ghapi.all import GhApi
 
 from collect_users.methods.github_search.github_search import get_complete_query_result, get_users_from_repos, get_users_from_users, Service
+from collect_users.methods.pure.pure import get_username_from_text
+
+
+"""
+Tests for github_search.py
+"""
 
 @pytest.fixture
 def service():
@@ -17,14 +23,12 @@ def service():
 def repos():
     repos = L(AttrDict(owner=AttrDict(login="kequach")), 
               AttrDict(owner=AttrDict(login="123")))
-
     return repos
 
 @pytest.fixture
 def users():
     users = L(AttrDict(login="kequach"), 
               AttrDict(login="123"))
-
     return users
 
 def test_get_users_from_repos(repos, service):
@@ -61,3 +65,42 @@ def test_get_complete_query_result_repos():
     service.api.last_page = Mock(return_value=0)
     result = get_complete_query_result("test_query", "SEARCH_REPOS", service) 
     assert result[0]["login"] == "kequach"
+
+
+"""
+Tests for pure.py
+"""
+
+@pytest.fixture
+def rispy_no_github():
+    rispy = {'type_of_reference': 'ADVS', 
+    'primary_title': 'ASReview Software Documentation.', 
+    'authors': ['de Bruin, J.', 'Ferdinands, G.', 'Harkema, A.', 'de Boer, J.', 'van den Brand, S.', 'Ma, Y.', 'van de Schoot, R.'], 
+    'year': '2021/1/18', 'publication_year': '2021/1/18', 
+    'doi': '10.5281/zenodo.4287119', 
+    'type_of_work': 'Software', 
+    'publisher': 'Zenodo'}
+    return rispy
+
+
+@pytest.fixture
+def rispy_github():
+    rispy = {'type_of_reference': 'ADVS', 
+    'primary_title': 'Birdwatcher', 
+    'secondary_authors': ['Mol, C.', 'ter Haar, Sita', 'Beckers, G.J.L.'], 
+    'year': '2018/11/30', 
+    'publication_year': '2018/11/30', 
+    'notes_abstract': 'Birdwatcher is a Python computer vision library for the analysis of animal behavior.It is developed by Gabriel Beckers, Sita ter Haar and Carien Mol, at Experimental Psychology, Utrecht University.It is being used in our lab but not stable enough yet for general use. More info will be provided when a first release is appropriate.Code can be found on GitHub: https://github.com/gbeckers/BirdwatcherIt is open source and freely available under the New BSD License terms.', 
+    'abstract': 'Birdwatcher is a Python computer vision library for the analysis of animal behavior.It is developed by Gabriel Beckers, Sita ter Haar and Carien Mol, at Experimental Psychology, Utrecht University.It is being used in our lab but not stable enough yet for general use. More info will be provided when a first release is appropriate.Code can be found on GitHub: https://github.com/gbeckers/BirdwatcherIt is open source and freely available under the New BSD License terms.', 
+    'type_of_work': 'Software'}
+    return rispy
+
+
+def test_get_username_from_text_empty(rispy_no_github):
+    user = get_username_from_text(rispy_no_github.values()) 
+    assert user is None
+
+
+def test_get_username_from_text_found(rispy_github):
+    user = get_username_from_text(rispy_github.values()) 
+    assert user == "gbeckers"
