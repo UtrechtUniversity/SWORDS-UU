1bb9e3cad9cda3319060f7ae7f6597ad59eab352
Keven Quach <belfami@gmail.com>
2022-06-29 14:19:56 +0200

    Update pipeline with more meaningful output

--- collect_users/run_pipeline.sh
+++ collect_users/run_pipeline.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 echo "Fetching users..."
 cd methods/github_search
-pip install -r requirements.txt
+pip -q install -r requirements.txt
 python github_search.py --topic utrecht-university --search "utrecht university"
+echo "Github search method finished."
 cd ../papers_with_code
-pip install -r requirements.txt
+pip -q install -r requirements.txt
 python papers_with_code.py --query utrecht+university
+echo "PapersWithCode search method finished."
 cd ../pure
-pip install -r requirements.txt
-python pure.py
+pip -q install -r requirements.txt
+python pure.py data/Pure_290421.ris
+echo "PURE search method finished."
 cd ../profile_pages
-pip install -r requirements.txt
+pip -q install -r requirements.txt
+echo "Starting UU API search method. This will take a while."
 python uu_api_crawler.py
+echo "UU API search method finished."
 echo "Merging users..."
 
 cd ../../
-pip install -r requirements.txt
+pip -q install -r requirements.txt
 python scripts/merge_users.py --files methods/*/results/*.csv --output results/users_merged.csv
 echo "Enriching users..."
 python scripts/enrich_users.py --input results/users_merged.csv
